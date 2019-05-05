7b4f2c2b7c05153214c13e19695ccb939fda6b6a
Andrew Millar <duckman.andrew@gmail.com>
2019-05-05 21:18:02 +0200

    Change some style and adjust the date function and change some text and links behaviour

--- src/index.php
+++ src/index.php
@@ -4,8 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Noverius</title>
-
+  <title>Noverius</title> 
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
   <link rel="stylesheet" href="./css/stars.css">
@@ -13,18 +12,21 @@
 </head>
 <body>
 <?php 
-$path = 'http://millar-knorr.nl/test/xml/accofeed.xml';
+$path = 'http://demo.travelpulse.nl/sites/default/files/feeds/accofeed.xml';
 $xml_file = file_get_contents($path);
 $ob = simplexml_load_string($xml_file, 'SimpleXMLElement', LIBXML_NOCDATA);
+$sort_variable = "";
 
-// Sorteer start, maak er nog een functie van
+// Sorteer start, maak er misschien nog een functie van, Class is over the top
 $sorteer = "";
-if( $_GET["sorteer"] === "prijs") {
-  $sorteer = htmlspecialchars($_GET["sorteer"]);
-  $sort_variable = "price";
-} else if ($_GET["sorteer"] === "naam") {
-  $sorteer = htmlspecialchars($_GET["sorteer"]);  
-  $sort_variable = "accommodation_name";
+if($_GET["sorteer"] ?? 0) {
+  if($_GET["sorteer"] && $_GET["sorteer"] === "prijs") {
+    $sorteer = htmlspecialchars($_GET["sorteer"]);
+    $sort_variable = "price";
+  } else if ($_GET["sorteer"] && $_GET["sorteer"] === "naam") {
+    $sorteer = htmlspecialchars($_GET["sorteer"]);  
+    $sort_variable = "accommodation_name";
+  }
 }
 
 // Maak een sorteerbare array uit het simplexml_load_string object
@@ -41,16 +43,12 @@ function sort_by($a, $b) {
 }
 
 usort($sortable, 'sort_by');
-// End sort
 
 $json = json_encode($sortable);
-// $json = json_encode($ob);
 $data_array = json_decode($json, true);
-// Catagories are not used but used as a reference
-$catagories = ["category", "description", "img_small", "link", "minimum_price", "departure_date", "title", "accommodation_name", "city_of_destination", "continent_of_destination", "country_of_destination", "accommodation_type", "holiday_type", "region_of_destination", "stars"];
 
 function formatItems($item) {
-  // Haal de data uit het item zodat het kan worden "behandeld"
+  // Stop de data uit het item in een bootstrap card
   // Dat htmlspecialchars gebeuren kan ongetwijfelt wat meer DRY
   return "<div class=\"card my-2\">
           <img class=\"card-img\" src=\"{$item["img_small"]}\">
@@ -62,7 +60,7 @@ function formatItems($item) {
             <p class=\"card-text\">" . htmlspecialchars($item["description"]) . "</p>
             <p class=\"card-text\">" . htmlspecialchars($item["city_of_destination"]) . " aan de " . htmlspecialchars($item["continent_of_destination"]) . " in " . htmlspecialchars($item["country_of_destination"]) . "</p>
             <P class\"float-right\">&euro;" . htmlspecialchars($item["minimum_price"]) . "</P>
-            <a href=\"" . htmlspecialchars($item["link"]) . "\" class=\"btn btn-outline-primary btn-block\">link</a>
+            <a href=\"" . htmlspecialchars($item["link"]) . "\" class=\"btn btn-primary btn-block\">Boek deze vakantie</a>
           </div>
           <div class=\"card-footer text-muted\">
             <div class=\"float-left\">Vertrek datum - " . formatDate($item["departure_date"]) . "</div>
@@ -75,16 +73,13 @@ function formatItems($item) {
         </div>";
 }
 
-
 function formatDate($str) {
-  // Maak van een 20190501 datum een "wendsday 1 ste mei 2019"
+  // Maak van een 20190501 datum een "woendsdag 1ste mei 2019"
   $date = new DateTime($str);
-  // $year = ;
+  $date = explode(", ", $date->format('d, m, Y'));
+  // Set de locale Nederlands
   setlocale(LC_ALL, 'nl_NL');
-  strftime("%A %d %B %Y", mktime(0, 0, 0, 12, 22, 1978));
-  return $date->format('l jS M Y');
-  // gebruik preg_replace() om de datum Nederlands te maken
-  // Zou ook moeten kunnen door de locale te veranderen
+  return strftime("%A %e %B %Y", mktime(0, 0, 0, $date[0], $date[1], $date[2]));
 }
 ?>
 
@@ -97,8 +92,8 @@ function formatDate($str) {
   <div class="row">
     <div class="btn-group mt-3" role="group" aria-label="Basic example">
       <button class="btn btn-light">sorteer op: </button>
-      <a href="/sandbox/src/index.php?sorteer=prijs" class="btn <?= $_GET["sorteer"] === "prijs" ? "btn-primary" : "btn-secondary" ?>">Prijs</a>
-      <a href="/sandbox/src/index.php?sorteer=naam" class="btn <?= $_GET["sorteer"] === "naam" ? "btn-primary" : "btn-secondary" ?>">Naam</a>
+      <a href="/sandbox/src/index.php<?= $sorteer === "prijs"? "": "?sorteer=prijs" ?>" class="btn <?= $_GET["sorteer"] === "prijs" ? "btn-primary" : "btn-secondary" ?>">Prijs</a>
+      <a href="/sandbox/src/index.php<?= $sorteer === "naam"? "": "?sorteer=naam" ?>" class="btn <?= $_GET["sorteer"] === "naam" ? "btn-primary" : "btn-secondary" ?>">Naam</a>
     </div>
     <?php 
       foreach ($data_array as $items) { 
@@ -108,8 +103,6 @@ function formatDate($str) {
     ?>
   </div>
 </main>
-
-
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
